7fb7f140d534d0b9d4219f1d9adf546eb73085b0
monicaarganda <argandamonica1@gmail.com>
2023-01-03 10:39:02 -0800

    As a user, when I click on one of the question marks an alert appears with the index position of that question mark in the array.

--- src/App.js
+++ src/App.js
@@ -15,15 +15,22 @@ const App = () => {
     "?"
   ])
 
+  const handleGamePlay = (clickSquare) => {
+    alert(clickSquare)
+  }
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board">
       {/* board is made up of square, just makes sense */}
-      {board.map(square => {
+      {board.map((square, index) => {
         return(
-        <Square square={square}/>
+        <Square 
+        square={square} 
+        index={index} 
+        handleGamePlay= {handleGamePlay}
+        />
         )
       })}
       </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,22 @@
 import React from "react"
 // destructuring props, do not need to write props and can pass mulitple variables inside parameters with separation of a comma
-const Square = ({ square }) => {
+const Square = ({ square, index, handleGamePlay }) => {
   // console.log(props)
+
+  const handleClick = () => {
+    handleGamePlay (index)
+  }
+  
   return (
     <>
-      <div className="square">{square}</div>
+      <div className="square" onClick={handleClick}>{square}</div>
     </>
   )
 }
 export default Square
+
+//step one:
+// Making the code more dyamic because the array is in app.js we need the alert to be called on in app.js because thats where the logic is. 
+// const handleClick = () => {
+//   alert (index)
+// }
